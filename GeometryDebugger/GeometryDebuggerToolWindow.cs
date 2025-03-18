e7cd7f30211537c63bfeec853e9afc00db3e10ee
MypkaXD <nicklayschannel@yandex.ru>
2025-03-18 11:35:32 +0300

    init

---
+++ GeometryDebugger/GeometryDebuggerToolWindow.cs
@@ -0,0 +1,40 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="GeometryDebuggerToolWindow.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace GeometryDebugger
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("e230b921-c80c-4386-961b-fa71138b4170")]
+    public class GeometryDebuggerToolWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GeometryDebuggerToolWindow"/> class.
+        /// </summary>
+        public GeometryDebuggerToolWindow() : base(null)
+        {
+            this.Caption = "GeometryDebuggerToolWindow";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new GeometryDebuggerToolWindowControl();
+        }
+    }
+}
